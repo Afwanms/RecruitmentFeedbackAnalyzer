d96b6f037b9b91859da7f2dfa6fbfaaa52f34c6b
Afwan Maulana Sidqi <afwanmaulanas02@gmail.com>
2026-06-28 22:20:46 +0700

    make schema for API data format

---
+++ backend/app/schemas.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+
+class CandidateCreate(BaseModel):
+    candidate_name: str
+    position: str
+
+class CandidateResponse(BaseModel):
+    candidate_id: int
+    candidate_name: str
+    position: str
+    status: str
+
+    class Config:
+        from_attributes = True
+
+class AnalyzeRequest(BaseModel):
+    candidate_id: int
+    feedback: str
+
+class AnalyzeResponse(BaseModel):
+    category: str
+
+class DashboardResponse(BaseModel):
+    total_candidate: int
+    total_feedback: int
+    top_category: str
+    candidates: list[CandidateResponse]

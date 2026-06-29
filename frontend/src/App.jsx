7b0ae5c5379e75567011827a5861ed0ac2026da0
Afwan Maulana Sidqi <afwanmaulanas02@gmail.com>
2026-06-30 00:19:35 +0700

    Finish make dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
 import Dashboard from "./pages/Dashboard";
+import CandidateDetail from "./pages/CandidateDetail";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route
+        path="/candidates/:candidateId"
+        element={<CandidateDetail />}
+      />
+    </Routes>
+  );
 }
 
 export default App;
